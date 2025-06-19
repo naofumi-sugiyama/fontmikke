468c2204613a5e73ecd627e6b675e1b84103448a
杉山　尚郁 <naofumi@MacBookPro.lan>
2025-06-19 14:16:31 +0900

    FIX entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,12 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
-# Run database migrations before starting the app
+echo "=== Running database migrations ==="
 bundle exec rails db:migrate
 
+echo "=== Precompiling assets ==="
+bundle exec rake assets:precompile
+
+echo "=== Starting the app ==="
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
