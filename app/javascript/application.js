4d913fd5df86dac2a29bb07f1864525ca415c17f
杉山　尚郁 <naofumi@MacBookPro.lan>
2025-07-22 00:02:12 +0900

    feat: フォントプレビュー機能のJavaScript実装

---
+++ app/javascript/application.js
@@ -0,0 +1,4 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "font_preview"
+
+console.log("Application JavaScript loaded with importmap!");
