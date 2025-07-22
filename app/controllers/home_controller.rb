253c57999221c0b92afff50b0a19ce3ba6ed1c5b
杉山　尚郁 <naofumi@MacBookPro.lan>
2025-07-23 00:26:12 +0900

    debug: Add font count display for production debugging

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
     @fonts = Font.all
+
+  # デバッグ用ログ出力
+  Rails.logger.info "=== Font count: #{@fonts.count} ==="
+
+  # 画面上にも表示（一時的）
+  flash.now[:notice] = "フォント数: #{@fonts.count}件"
   end
 end
