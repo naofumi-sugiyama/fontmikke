e6126fad1d3f5ff301eca399cf33f5ac36b9a003
杉山　尚郁 <naofumi@sugiyamanaokaorunoMacBook-Pro.local>
2025-08-10 14:42:59 +0900

    利用規約の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/terms'
+  get 'static_pages/privacy'
   root "home#index"
 
   resources :fonts, only: [:show]
+
+  # 静的ページ
+  get 'terms', to: 'static_pages#terms'
+  get 'privacy', to: 'static_pages#privacy'
 end
