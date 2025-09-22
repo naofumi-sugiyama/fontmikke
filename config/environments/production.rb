6f4b572a3a98084b6288892f25a2e6723b002522
naofumi-sugiyama <sugiyama.naofumi.09@gmail.com>
2025-09-22 21:22:31 +0900

    Fix syntax error: add missing comma in smtp_settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
     user_name: ENV['GMAIL_USERNAME'],
     password: ENV['GMAIL_PASSWORD'],
     authentication: 'plain',
-    enable_starttls_auto: true
+    enable_starttls_auto: true,
     openssl_verify_mode: 'none'
   }
 
