01fd79021f510aac1d7ab75dc1155270e251c297
naofumi-sugiyama <sugiyama.naofumi.09@gmail.com>
2025-09-24 22:55:47 +0900

    Fix sorcery initializer: replace 24.hours with 86400

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -5,9 +5,8 @@ Rails.application.config.sorcery.configure do |config|
     user.stretches = 1 if Rails.env.test?
 
     user.reset_password_mailer = UserMailer
-    user.reset_password_time_between_emails = 5 * 60
-
-    user.reset_password_expiration_period = 24.hours
+    user.reset_password_time_between_emails = 300
+    user.reset_password_expiration_period = 86400
   end
 
   config.user_class = "User"
